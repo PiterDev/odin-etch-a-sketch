c10b74d33ddade0d6e580672dc136fda5ec846b6
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-01 08:35:51 +0200

    Create hover effect

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+const container_size = 720;
+
 document.addEventListener("DOMContentLoaded", () => {
-    createSquareDivs(16);
+    createSquareDivs(32);
+    document.querySelectorAll(".etch-div").forEach(div => {
+        div.addEventListener("mouseover", divMouseOver)
+    });
 })
+
+function divMouseOver(event) {
+    this.style.backgroundColor = "#000";
+}
 
 function createSquareDivs(gridSize) {
     let etchContainer = document.querySelector(".container");
-    gridSize **= 2
-    for (let i = 0; i<gridSize; i++) {
+    let divAmount = gridSize ** 2;
+    for (let i = 0; i < divAmount; i++) {
         let div = document.createElement("div");
+        div.classList.add("etch-div");
+        
         etchContainer.appendChild(div);
+        div.style.flexBasis = `${container_size/gridSize}px`;
+        //div.style.height = `${container_size/gridSize}px`;
 
     }
 }
