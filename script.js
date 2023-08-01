7c0def5f24508015220a1752ace889cc15d3856d
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-01 08:54:20 +0200

    Add working reset button

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const container_size = 720;
 
 document.addEventListener("DOMContentLoaded", () => {
     createSquareDivs(32);
-    document.querySelectorAll(".etch-div").forEach(div => {
-        div.addEventListener("mouseover", divMouseOver)
-    });
+    document.querySelector(".generate").addEventListener("click", () => {
+        let containerSize = 0;
+        while (containerSize <= 0 || containerSize > 100) {
+            containerSize = prompt("How much squares per side? (max 100)", 10);
+        }
+        
+        createSquareDivs(containerSize);
+    })
 })
 
 function divMouseOver(event) {
@@ -13,14 +18,14 @@ function divMouseOver(event) {
 
 function createSquareDivs(gridSize) {
     let etchContainer = document.querySelector(".container");
+    etchContainer.replaceChildren();
     let divAmount = gridSize ** 2;
     for (let i = 0; i < divAmount; i++) {
         let div = document.createElement("div");
         div.classList.add("etch-div");
-        
+        div.addEventListener("mouseover", divMouseOver);
+
         etchContainer.appendChild(div);
         div.style.flexBasis = `${container_size/gridSize}px`;
-        //div.style.height = `${container_size/gridSize}px`;
-
     }
 }
